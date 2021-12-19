d59c66e04631bfbd2af192b8e8b35bf9727702dc
萧 <1243925457@qq.com>
2021-12-19 16:36:34 +0800

    项目初始化

---
+++ src/main/java/com/cloud/war/cloudwar/CloudWarApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.war.cloudwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/war/cloudwar/CloudWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.war.cloudwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
